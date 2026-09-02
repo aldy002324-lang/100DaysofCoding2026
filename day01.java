31220b0d277938e6a9aade51d7ba8e86f3b4930d
aldy002324-lang <aldy002324@gmail.com>
2026-09-02 22:08:08 +0800

    day01.java

---
+++ day01.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Menampilkan teks ke layar
+
+        /*
+         * Perintah di bawah ini
+         * digunakan untuk mencetak
+         * tulisan Hello World
+         */
+
+        System.out.println("Hello World");
+    }
+}
+
+/**
+ * Program sederhana Java
+ * untuk memahami struktur dasar kode
+ */

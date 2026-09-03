818b1fe07f87749238f5bb4a5d7b47e72a15c05b
aldy002324-lang <aldy002324@gmail.com>
2026-09-03 20:45:12 +0800

    day02.java

---
+++ day02.java
@@ -0,0 +1,12 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Muhammad Aldi";
+        int umur = 19;
+        double tinggi = 165.2;
+        System.out.printf("nama saya %s saya berumur %d dan tinggi saya itu %.1f \n" ,nama,umur,tinggi);
+        System.out.println("nama : "+ nama);
+        System.out.print("umur  "+ umur);
+    }
+    
+}

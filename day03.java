6938ef4400b691c4a63e4619cc9ec14582b4e2b8
aldy002324-lang <aldy002324@gmail.com>
2026-09-04 19:40:56 +0800

    day03.java

---
+++ day03.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        
+        System.out.println("===================================");
+        System.out.println("\t TOKO BUKU : \"GARUDA\"");
+        System.out.println("===================================");
+        System.out.println("Promo Hari ini : \"beli 2 Gratis 1\"\n");
+        System.out.println("\nItem\tHarga");
+        System.out.println("Novel\tRp55.000");
+        System.out.println("Komik\tRp25.000");
+        System.out.println("Atlas\tRp80.000");
+        System.out.println("\nFile struk disimpan di: D:\\Toko\\Struk");
+        System.out.println("Terima kasih sudah belanjda di Toko GARUDA");
+    }
+}

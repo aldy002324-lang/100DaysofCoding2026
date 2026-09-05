daca23c448bec737a7ceb0322c99e2f713d9f30e
aldy002324-lang <aldy002324@gmail.com>
2026-09-05 23:11:43 +0800

    day04.java

---
+++ day04.java
@@ -0,0 +1,24 @@
+public class day04 {
+    public static void main(String[] args) {
+
+        byte nilaiByte = 100;
+
+        short nilaiShort = 10000;
+
+        int nilaiInt = 1000000;
+
+        long nilaiLong = 10000000000L;
+
+        System.out.println("Nilai byte  : " + nilaiByte);
+        System.out.println("Nilai short : " + nilaiShort);
+        System.out.println("Nilai int   : " + nilaiInt);
+        System.out.println("Nilai long  : " + nilaiLong);
+    }
+}
+// Tipe data byte digunakan untuk menyimpan bilangan bulat kecil
+// Tipe data short digunakan untuk menyimpan bilangan bulat
+// dengan kapasitas lebih besar dari byte
+// Tipe data int digunakan untuk menyimpan bilangan bulat
+// Tipe data long digunakan untuk menyimpan bilangan bulat
+// yang sangat besar
+// Menampilkan nilai dari masing-masing tipe data

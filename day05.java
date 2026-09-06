eb1e0cdf9159cfe5ae25708295fef46d590af187
aldy002324-lang <aldy002324@gmail.com>
2026-09-06 22:18:03 +0800

    day05.java

---
+++ day05.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) {
+        float umur = 19f;
+        double tinggi = 165.4;
+
+        System.out.println("umur :" + umur);
+        System.out.println("tinggi :" + tinggi);
+
+        // tipe data float digunakan untuk menyimpan bilangan pecahan dengan ketelitian lebih rendah
+        // huruf f dibelakang angka menandakan nilai float
+        // tipe data double digunakan untuk menyimpan bilangan pecahan dengan ketelitian lebih tinggi
+
+        // menampilkan nilai dari variabel float 
+        // menampilkan nilai dari variabel double
+    }
+}

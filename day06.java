865ab636381131f034ff8870be44b0c643cb8120
aldy002324-lang <aldy002324@gmail.com>
2026-09-07 21:27:53 +0800

    day06.java

---
+++ day06.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        
+        char huruf = 'A';
+
+        boolean lulus = true;
+
+        // menampilkan nilai char atau boolean
+        System.out.println("huruf :" + huruf);
+        System.out.println("setelah lulus :" + lulus);
+
+        // tipe data char digunakan untuk menyimpan satu karakter
+        // tipe data boolean digunakan untuk menyimpan nilai true atau false
+        
+
+    }
+}

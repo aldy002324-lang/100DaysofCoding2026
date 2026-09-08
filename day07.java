8c7e90605a336abba213979c1ca59a309f2ac250
aldy002324-lang <aldy002324@gmail.com>
2026-09-08 23:05:43 +0800

    day07.java

---
+++ day07.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String nama = "Muhammad Aldi"; // Menyimpan teks/nama
+        String jurusan = "Teknik Informatika"; // Menyimpan nama jurusan
+
+        System.out.println(nama);
+        System.out.println(jurusan);
+    }
+}

b845ed621f121b3f6d7552c3d0620efda936a21f
aldy002324-lang <aldy002324@gmail.com>
2026-09-09 23:19:17 +0800

    day08.java

---
+++ day08.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) {
+
+        // Deklarasi variabel
+        int umur;
+        String nama;
+
+        // Inisialisasi variabel
+        umur = 19;
+        nama = "Aldi";
+
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur awal : " + umur);
+
+        // Update variabel
+        umur = 20;
+        System.out.println("Umur setelah update : " + umur);
+    }
+}

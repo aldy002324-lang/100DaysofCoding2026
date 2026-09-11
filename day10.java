945c5d208e07b35379b2fc64e8bda3f5a83c3210
aldy002324-lang <aldy002324@gmail.com>
2026-09-11 23:22:42 +0800

    day10.java

---
+++ day10.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) {
+        
+        System.out.println("Byte min : " + Byte.MIN_VALUE);
+        System.out.println("Byte max : " + Byte.MAX_VALUE);
+
+        System.out.println("Short min : " + Short.MIN_VALUE);
+        System.out.println("Short max : " + Short.MAX_VALUE);
+
+        System.out.println("Int min : " + Integer.MIN_VALUE);
+        System.out.println("Int max : " + Integer.MAX_VALUE);
+
+        System.out.println("Long min : " + Long.MIN_VALUE);
+        System.out.println("Long max : " + Long.MAX_VALUE);
+
+        System.out.println("Float min : " + Float.MIN_VALUE);
+        System.out.println("Float max : " + Float.MAX_VALUE);
+
+        System.out.println("Double min : " + Double.MIN_VALUE);
+        System.out.println("Double max : " + Double.MAX_VALUE);
+
+        System.out.println("Char min : " + (int) Character.MIN_VALUE);
+        System.out.println("Char max : " + (int) Character.MAX_VALUE);
+
+        System.out.println("Boolean : false atau true");
+
+        // min adalah nilai terkecil yang dapat disimpan oleh tipe data, 
+        // Sedangkan max adalah nilai terbesar yang dapat di simpan.
+        // batas ini berbeda pada setiap data
+    }
+}

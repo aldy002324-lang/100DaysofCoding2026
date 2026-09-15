3e0babdf83b25d7106e19f55009487073b6a4259
aldy002324-lang <aldy002324@gmail.com>
2026-09-15 22:35:21 +0800

    day14.java

---
+++ day14.java
@@ -0,0 +1,13 @@
+public class day14 {
+    public static void main(String[] args) {
+
+        int a = 20; // Nilai pertama
+        int b = 8;  // Nilai kedua
+
+        int penjumlahan = a + b; // Menjumlahkan a dan b
+        int pengurangan = a - b; // Mengurangi a dengan b
+
+        System.out.println("Hasil Penjumlahan = " + penjumlahan);
+        System.out.println("Hasil Pengurangan = " + pengurangan);
+    }
+}

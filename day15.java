99eedb6a492354a616e61b2aa59764e8fd28e3b3
aldy002324-lang <aldy002324@gmail.com>
2026-09-16 23:27:50 +0800

    day15.java

---
+++ day15.java
@@ -0,0 +1,16 @@
+public class day15 {
+    public static void main(String[] args) {
+        
+        //soal 3
+        int a = 50;
+        int b = 30;
+        int hasil = a * b;
+        System.out.println(hasil);
+
+        //soal 4
+        int c = 40;
+        int d = 20;
+        int jumlah = c / d;
+        System.out.println(jumlah);
+    }
+}

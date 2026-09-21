2593cfd722d18c5d647f188192da16dce9ed7edf
aldy002324-lang <aldy002324@gmail.com>
2026-09-21 23:19:05 +0800

    day20.java

---
+++ day20.java
@@ -0,0 +1,15 @@
+public class 20 {
+    public static void main(String[] args) {
+        int angka = 20;
+        double nilai = 17.5;
+        boolean benar = true;
+
+        String a = String.valueOf(angka);
+        String b = String.valueOf(nilai);
+        String c = String.valueOf(benar);
+
+        System.out.println("int ke String    : " + a);
+        System.out.println("double ke String : " + b);
+        System.out.println("boolean ke String: " + c);
+    }
+}
